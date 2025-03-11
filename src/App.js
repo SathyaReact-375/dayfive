4defcc73ff0165def89b01cd3188b8adb1761c00
sathyasiva735@gmail.com <sathyasiva735@gmail.com>
2025-03-11 15:52:23 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Task1 from './Components/Task1'
 
 function App() {
   return (
     <div>
-      <h1>hii this is testing message</h1>
+      <h1 style={{textAlign:"center"}}>1.List Rendering Introduction</h1>
+      <Task1/>
     </div>
   )
 }
---
+++ src/Components/Task1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Task1() {
+  return (
+    <div>
+        <div style={{textAlign:"center"}}>
+  <p>In React, lists can be efficiently rendered using the <code>map()</code> function. This function iterates over an array and creates a new array of elements. By leveraging the <code>map()</code> function, you can dynamically generate a list of elements from your data. Each element in the list should have a unique <code>key</code> prop to ensure React can efficiently update and manage the list.</p>
+</div>
+
+    </div>
+  )
+}
+
+export default Task1
